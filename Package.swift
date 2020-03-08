810c9790267424797fba9d6ed415f30306991514
Joe Thomson <joseph_2166@mac.com>
2020-03-08 10:41:38 +0000

    Added Swift Package Manager support

---
+++ Package.swift
@@ -0,0 +1,18 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "ColorCompatibility",
+    platforms: [
+        .iOS(.v8),
+        .tvOS(.v9),
+    ],
+    products: [
+        .library(name: "ColorCompatibility", targets: ["ColorCompatibility"]),
+    ],
+    targets: [
+        .target(name: "ColorCompatibility", path: "Sources"),
+    ],
+    swiftLanguageVersions: [.v5]
+)
